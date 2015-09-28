02979a9b5f39fdc1be02e9e9410393445cfd6f9f
Blake Tidwell <batidwell@gmail.com>
2015-09-27 17:55:52 -0700

    Refactor to shelljs

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,6 +1,5 @@
+require('shelljs/global');
 var fs = require('fs');
-var util = require('util')
-var exec = require('child_process').exec;
 var ejs = require('ejs');
 var tplStr, compiledStr;
 
@@ -9,6 +8,7 @@ compiledStr = ejs.render(tplStr, { appname: 'test' });
 
 fs.writeFileSync('./test/fixtures/app/package.json', compiledStr);
 
-function puts(error, stdout, stderr) { console.log(stdout) }
-process.chdir('./test/fixtures/app');
-exec('npm install', puts);
+cd('./test/fixtures/app');
+exec('npm install', function(code, output) {
+  console.log(output);
+});
